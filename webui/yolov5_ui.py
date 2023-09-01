9312ae403b5ef0893968e095bca7b21130c1b6d3
isLinXu <2267379130@qq.com>
2023-09-02 05:25:18 +0800

    [webui] update yolov5_ui.py

--- webui/yolov5_ui.py
+++ webui/yolov5_ui.py
@@ -1,13 +1,14 @@
-import gradio as gr
 import torch
 import cv2
 from PIL import Image
 import numpy as np
+import gradio as gr
+import warnings
+warnings.filterwarnings("ignore")
 
 class YOLOv5Detector:
     def __init__(self):
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/yolov5/yolov5s.pt')
-        self.model.conf = 0.25
+        pass
 
     def detect_objects(self, img):
         # Convert input image to numpy array
@@ -37,14 +38,69 @@ class YOLOv5Detector:
 
         return img
 
+    def detect_objects_ss(self, img, conf, iou, line_width, device, model_type, model_path):
+        if model_type == "yolov5n":
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/yolov5/yolov5n.pt')
+        elif model_type == "yolov5s":
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/yolov5/yolov5s.pt')
+        elif model_type == "yolov5m":
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/yolov5/yolov5m.pt')
+        elif model_type == "yolov5l":
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/yolov5/yolov5l.pt')
+        elif model_type == "yolov5x":
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/yolov5/yolov5x.pt')
+
+        if model_type not in ["yolov5n", "yolov5s", "yolov5m", "yolov5l", "yolov5x"]:
+            self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
+
+        # Convert input image to numpy array
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Convert numpy array to PIL image
+        img = Image.fromarray(img)
+        # Use YOLOv5 model for inference
+        results = self.model(img)
+        xyxy = results.pandas().xyxy[0]
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        # Iterate through each bbox, draw bounding box and label name
+        for bbox in xyxy.itertuples():
+            # Generate random color
+            color = tuple(np.random.randint(0, 255, 3).tolist())
+            xmin, ymin, xmax, ymax = bbox[1:5]
+            xmin = int(xmin)
+            ymin = int(ymin)
+            xmax = int(xmax)
+            ymax = int(ymax)
+            confidence = bbox[5]
+            name = bbox[7]
+            if confidence > conf:
+                # Draw bounding box
+                cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color, line_width)
+                # Draw label name
+                label = f"{name}: {confidence:.2f}"
+                cv2.putText(img, label, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, line_width)
+
+        return img
+
+
+
 if __name__ == '__main__':
     # Instantiate YOLOv5Detector class
     detector = YOLOv5Detector()
 
     # Define Gradio interface
     iface = gr.Interface(
-        fn=detector.detect_objects,
-        inputs="image",
+        fn=detector.detect_objects_ss,
+        # inputs="image",
+        inputs=["image",
+                gr.inputs.Slider(minimum=0, maximum=1, step=0.01, default=0.25,
+                                 label="Confidence Threshold"),
+                gr.inputs.Slider(minimum=0, maximum=1, step=0.01, default=0.45,
+                                 label="IoU Threshold"),
+                gr.inputs.Number(default=3, label="Line Width"),
+                gr.inputs.Radio(["cpu", "cuda"], label="Device", default="cpu"),
+                gr.inputs.Radio(["yolov5s", "yolov5m", "yolov5l", "yolov5x"],
+                                label="Model Type", default="yolov5s"),
+                gr.inputs.Textbox(default="yolov5s.pt", label="Model Path")],
         outputs="image",
         title="YOLOv5 Object Detector",
         description="Detect objects in an image using YOLOv5 model.",
