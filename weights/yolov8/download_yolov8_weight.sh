48b6a01faa65d5d4df43b347da19c2769fe9c0c4
isLinXu <2267379130@qq.com>
2023-09-03 13:06:26 +0800

    [weights][yolov8] add download_yolov8_weight.sh

---
+++ weights/yolov8/download_yolov8_weight.sh
@@ -0,0 +1,52 @@
+# https://github.com/ultralytics/assets/releases/tag/v0.0.0
+echo "Downloading YOLOv8-n weights..."
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt
+echo "Download YOLOv8 yolov8n weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n-cls.pt
+echo "Download YOLOv8 yolov8n-cls weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n-pose.pt
+echo "Download YOLOv8 yolov8n-pose weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n-seg.pt
+
+echo "Downloading YOLOv8-s weights..."
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8s.pt
+echo "Download YOLOv8 yolov8s weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8s-cls.pt
+echo "Download YOLOv8 yolov8s-cls weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8s-pose.pt
+echo "Download YOLOv8 yolov8s-pose weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8s-seg.pt
+echo "Download YOLOv8 yolov8s-seg weights done!"
+
+echo "Downloading YOLOv8-m weights..."
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8m.pt
+echo "Download YOLOv8 yolov8m weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8m-cls.pt
+echo "Download YOLOv8 yolov8m-cls weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8m-pose.pt
+echo "Download YOLOv8 yolov8m-pose weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8m-seg.pt
+echo "Download YOLOv8 yolov8m-seg weights done!"
+
+echo "Downloading YOLOv8-l weights..."
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8l.pt
+echo "Download YOLOv8 yolov8l weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8l-cls.pt
+echo "Download YOLOv8 yolov8l-cls weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8l-pose.pt
+echo "Download YOLOv8 yolov8l-pose weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8l-seg.pt
+echo "Download YOLOv8 yolov8l-seg weights done!"
+
+
+echo "Downloading YOLOv8x weights..."
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8x.pt
+echo "Download YOLOv8 yolov8x weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8x6.pt
+echo "Download YOLOv8 yolov8x6 weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8x-cls.pt
+echo "Download YOLOv8 yolov8x-cls weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8x-pose.pt
+echo "Download YOLOv8 yolov8x-pose weights done!"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8x-seg.pt
+echo "Download YOLOv8 yolov8x-seg weights done!"
