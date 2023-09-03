3a17a3e352663d9ce06fe6db585561d97158c537
isLinXu <2267379130@qq.com>
2023-09-03 19:54:36 +0800

    [weights][yolonas] add download_yolonas_weight.sh

---
+++ weights/yolonas/download_yolonas_weight.sh
@@ -0,0 +1,7 @@
+# download yolo_nas weights
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolo_nas_s.pt
+acho "yolo_nas_s.pt download done"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolo_nas_m.pt
+acho "yolo_nas_m.pt download done"
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/yolo_nas_l.pt
+acho "yolo_nas_l.pt download done"
