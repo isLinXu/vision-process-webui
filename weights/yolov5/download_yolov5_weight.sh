998525fd8edf558c365de56a2c7abef8ab462665
isLinXu <2267379130@qq.com>
2023-09-03 13:05:19 +0800

    [weights][yolov5] add download_yolov5_weight.sh

---
+++ weights/yolov5/download_yolov5_weight.sh
@@ -0,0 +1,27 @@
+echo "Downloading YOLOv5-n weights..."
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5n.pt
+echo "Download YOLOv5 yolov5n weights done!"
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5n6.pt
+echo "Download YOLOv5 yolov5n6 weights done!"
+
+echo "Downloading YOLOv5-s weights..."
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5s.pt
+echo "Download YOLOv5 yolov5s weights done!"
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5s6.pt
+echo "Download YOLOv5 yolov5s6 weights done!"
+
+echo "Downloading YOLOv5-m weights..."
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5m.pt
+echo "Download YOLOv5 yolov5m weights done!"
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5m6.pt
+echo "Download YOLOv5 yolov5m6 weights done!"
+
+echo "Downloading YOLOv5-l weights..."
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5l.pt
+echo "Download YOLOv5 yolov5l weights done!"
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5l6.pt
+echo "Download YOLOv5 yolov5l6 weights done!"
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5l-cls.pt
+echo "Download YOLOv5 yolov5l-cls weights done!"
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5l-seg.pt
+echo "Download YOLOv5 yolov5l-seg weights done!"
