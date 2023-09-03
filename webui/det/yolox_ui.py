1caa13c6ced516995cd3490270dc41c09a276550
isLinXu <2267379130@qq.com>
2023-09-04 02:15:21 +0800

    [webui][det] add yolox_ui.py

--- webui/det/yolox_ui.py
+++ webui/det/yolox_ui.py
@@ -1,7 +1,5 @@
 
-import torch
 import cv2
-from PIL import Image
 import numpy as np
 import gradio as gr
 
@@ -9,21 +7,25 @@ from super_gradients.training import models
 import warnings
 
 warnings.filterwarnings("ignore")
-class YOLO_NAS_WebUI:
+class YOLOX_WebUI:
     def __init__(self):
         pass
 
     def predict(self, image_path,conf, iou, line_width, device, model_type, model_path):
         self.device = device
-        if model_type == "yolo_nas_s":
-            self.model = models.get("yolo_nas_s", pretrained_weights="coco").to(self.device)
-        elif model_type == "yolo_nas_m":
-            self.model = models.get("yolo_nas_m", pretrained_weights="coco").to(self.device)
-        elif model_type == "yolo_nas_l":
-            self.model = models.get("yolo_nas_l", pretrained_weights="coco").to(self.device)
+        if model_type == "yolox_n":
+            self.model = models.get("yolox_n", pretrained_weights="coco").to(self.device)
+        elif model_type == "yolox_s":
+            self.model = models.get("yolox_s", pretrained_weights="coco").to(self.device)
+        elif model_type == "yolox_m":
+            self.model = models.get("yolox_m", pretrained_weights="coco").to(self.device)
+        elif model_type == "yolox_t":
+            self.model = models.get("yolox_t", pretrained_weights="coco").to(self.device)
+        elif model_type == "yolox_l":
+            self.model = models.get("yolox_l", pretrained_weights="coco").to(self.device)
         else:
             self.model = models.get(model_path, pretrained_weights="coco").to(self.device)
-        if model_type not in ["yolo_nas_s", "yolo_nas_m", "yolo_nas_l"]:
+        if model_type not in ["yolox_n", "yolox_s", "yolox_m", "yolox_l","yolox_t"]:
             self.model = models.get(model_path, pretrained_weights="coco").to(self.device)
 
         results = self.model.predict(image_path)
@@ -35,7 +37,6 @@ class YOLO_NAS_WebUI:
         bboxes_xyxy = prediction.bboxes_xyxy
         labels = prediction.labels
         confidences = prediction.confidence
-
         # draw rectangles and label names
         for bbox, label, confidence in zip(bboxes_xyxy, labels, confidences):
             color = tuple(np.random.randint(0, 255, 3).tolist())
@@ -50,7 +51,7 @@ class YOLO_NAS_WebUI:
 
 if __name__ == '__main__':
     # Instantiate YOLO_NAS_WebUI class
-    detector = YOLO_NAS_WebUI()
+    detector = YOLOX_WebUI()
 
     # Define Gradio interface
     iface = gr.Interface(
@@ -62,11 +63,11 @@ if __name__ == '__main__':
                                  label="IoU Threshold"),
                 gr.inputs.Number(default=2, label="Line Width"),
                 gr.inputs.Radio(["cpu", "cuda"], label="Device", default="cpu"),
-                gr.inputs.Radio(["yolo_nas_s", "yolo_nas_m", "yolo_nas_l"],
+                gr.inputs.Radio(["yolox_n", "yolox_s", "yolox_m", "yolox_l","yolox_t"],
                                 label="Model Type", default="yolo_nas_s"),
-                gr.inputs.Textbox(default="yolo_nas_s.pt", label="Model Path")],
+                gr.inputs.Textbox(default="yolox_n.pt", label="Model Path")],
         outputs="image",
-        title="YOLO-NAS Object Detector",
+        title="YOLOX_WebUI Object Detector",
         description="Detect objects in an image using YOLO-NAS model.",
         theme="default",
         layout="vertical",
