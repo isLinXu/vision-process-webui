89365c37dfe7ffa45c1340be396d51f45badff0c
isLinXu <2267379130@qq.com>
2023-09-03 14:37:25 +0800

    [webui] update mobilesam_ui.py

--- webui/mobilesam_ui.py
+++ webui/mobilesam_ui.py
@@ -1,37 +1,76 @@
 import cv2
 import gradio as gr
 import numpy as np
+from PIL.Image import Image
 from ultralytics import SAM
 import warnings
 
 warnings.filterwarnings("ignore")
 
 class SAMModel:
-    def __init__(self, model_path):
+    def __init__(self):
+        model_path = 'mobile_sam.pt'
         self.model = SAM(model_path)
 
-    def predict(self, image, x, y):
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    def mobilesam_point_predict(self, image, x, y):
         result = self.model.predict(image, points=[x, y], labels=[1])
-        result = result[0]
         plotted = result[0].plot()
+        plotted = cv2.cvtColor(np.array(plotted), cv2.COLOR_BGR2RGB)
         return plotted
 
-def sam_demo(model_path: str, title: str, description: str) -> None:
-    sam_model = SAMModel(model_path)
+    def mobile_bbox_predict(self, image: Image, bbox: str) -> np.ndarray:
+        # Parse the bounding box string
+        bbox_list = list(map(int, bbox.split(',')))
 
-    inputs = [
-        gr.inputs.Image(type='pil', label='Input Image'),
-        gr.inputs.Number(default=900, label='X Coordinate'),
-        gr.inputs.Number(default=370, label='Y Coordinate'),
-    ]
+        # Predict a segment based on a box prompt
+        result = self.model.predict(image, bboxes=bbox_list)
+        plotted = result[0].plot()
+        plotted = cv2.cvtColor(np.array(plotted), cv2.COLOR_BGR2RGB)
+        return plotted
 
-    output = gr.outputs.Image(type='pil', label='Output Image')
 
-    app = gr.Interface(fn=sam_model.predict, inputs=inputs, outputs=output,
-                       title=title,
-                       description=description)
+    def launch(self):
+        """Launches the Gradio interface."""
+        # Create the UI
+        with gr.Blocks() as app:
+            # Header
+            gr.Markdown("# SAM Model Demo")
 
-    app.launch()
+            # Tabs
+            with gr.Tabs():
+                # Point-predict-button Tab
+                with gr.TabItem("point-predict"):
+                    inputs = [
+                        gr.inputs.Image(type='pil', label='Input Image'),
+                        gr.inputs.Number(default=900, label='X Coordinate'),
+                        gr.inputs.Number(default=370, label='Y Coordinate'),
+                    ]
+                    # output = gr.Image()
+                    output = gr.outputs.Image(type='pil', label='Output Image')
+                    point_predict_button = gr.Button("inference")
+
+                    # Run object detection on the input image when the button is clicked
+                    point_predict_button.click(self.mobilesam_point_predict,
+                                           inputs=inputs,
+                                           outputs=output)
+
+                # Bbox-predict-button Tab
+                with gr.TabItem("bbox-predict"):
+                    image_input = gr.inputs.Image(type='pil')
+                    text_input = gr.inputs.Textbox(lines=1, label="Bounding Box (x1, y1, x2, y2)")
+                    image_output = gr.outputs.Image('pil')
+                    inputs = [image_input, text_input]
+                    output = image_output
+                    point_predict_button = gr.Button("inference")
+
+                    # Run object detection on the input image when the button is clicked
+                    point_predict_button.click(self.mobile_bbox_predict,
+                                               inputs=inputs,
+                                               outputs=output)
+
+                app.launch(share=True)
+
+
 if __name__ == '__main__':
-    sam_demo('mobile_sam.pt', 'SAM Model Demo', 'Use the SAM model to predict a segment based on a point prompt.')
+    web_ui = SAMModel()
+    web_ui.launch()
