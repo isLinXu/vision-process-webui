1a5cdaff739755cdb6899877750f324588fad4c6
isLinXu <2267379130@qq.com>
2023-09-03 13:05:05 +0800

    [weights][yolov3] add download_yolov3_weight.sh

---
+++ weights/yolov3/download_yolov3_weight.sh
@@ -0,0 +1,7 @@
+echo "Downloading YOLOv3 weights..."
+wget https://github.com/ultralytics/yolov3/releases/download/v9.6.0/yolov3-spp.pt
+echo "Download YOLOv3 yolov3-spp weights done!"
+wget https://github.com/ultralytics/yolov3/releases/download/v9.0/yolov3.pt
+echo "Download YOLOv3 yolov3 weights done!"
+wget https://github.com/ultralytics/yolov3/releases/download/v9.0/yolov3-tiny.pt
+echo "Download YOLOv3 yolov3-tiny weights done!"
