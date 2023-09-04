76243de6ae5fda033fc650a616a9b9dd27d78dcb
isLinXu <2267379130@qq.com>
2023-09-05 00:14:38 +0800

    [weights][mobile_sam] update download_mobilesam_weights.sh

--- weights/mobile_sam/download_mobilesam_weights.sh
+++ weights/mobile_sam/download_mobilesam_weights.sh
@@ -1,6 +1,4 @@
 # https://github.com/ultralytics/assets/releases/tag/v0.0.0
-echo "Downloading FastSAM-s weights..."
-wget https://github.com/ultralytics/assets/releases/download/v0.0.0/FastSAM-s.pt
-echo "Download FastSAM-s weights done!"
-wget https://github.com/ultralytics/assets/releases/download/v0.0.0/FastSAM-x.pt
-echo "Download FastSAM-x weights done!"
+echo "Downloading Mobile-Sam- weights..."
+wget https://github.com/ultralytics/assets/releases/download/v0.0.0/mobile_sam.pt
+echo "Download Mobile-Sam weights done!"
