df053f841b6d7f2e4f8ac30787016abd1148baee
isLinXu <2267379130@qq.com>
2023-09-08 02:17:53 +0800

    [webui][det][mmyolo] update mmyolo_ui.py

--- webui/det/mmyolo/mmyolo_ui.py
+++ webui/det/mmyolo/mmyolo_ui.py
@@ -1,3 +1,5 @@
+import fnmatch
+import glob
 import os
 import PIL.Image
 import cv2
@@ -18,6 +20,28 @@ from mmyolo.utils.labelme_utils import LabelmeFormat
 from mmyolo.utils.misc import get_file_list, show_data_classes
 from mim import download
 
+model_list = ['yolov5_n-v61_syncbn_fast_8xb16-300e_coco', 'yolov5_s-v61_syncbn_fast_8xb16-300e_coco',
+              'yolov5_n-p6-v62_syncbn_fast_8xb16-300e_coco', 'yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco',
+              'yolov5_n-v61_fast_1xb64-50e_voc', 'yolov5_s-v61_fast_1xb64-50e_voc',
+              'yolov5_ins_n-v61_syncbn_fast_8xb16-300e_coco_instance',
+              'yolov6_s_syncbn_fast_8xb32-400e_coco', 'yolov6_n_syncbn_fast_8xb32-400e_coco',
+              'yolox_tiny_fast_8xb8-300e_coco',
+              'yolox_s_fast_8xb8-300e_coco', 'yolox_m_fast_8xb8-300e_coco',
+              'yolox_tiny_fast_8xb32-300e-rtmdet-hyp_coco',
+              'yolox-pose_tiny_8xb32-300e-rtmdet-hyp_coco', 'yolox-pose_s_8xb32-300e-rtmdet-hyp_coco',
+              'rtmdet_tiny_syncbn_fast_8xb32-300e_coco',
+              'kd_tiny_rtmdet_s_neck_300e_coco', 'kd_s_rtmdet_m_neck_300e_coco',
+              'rtmdet_s_syncbn_fast_8xb32-300e_coco',
+              'rtmdet_m_syncbn_fast_8xb32-300e_coco',
+              'kd_m_rtmdet_l_neck_300e_coco',
+              'yolov7_tiny_syncbn_fast_8x16b-300e_coco', 'yolov7_l_syncbn_fast_8x16b-300e_coco',
+              'ppyoloe_plus_s_fast_8xb8-80e_coco',
+              'ppyoloe_plus_m_fast_8xb8-80e_coco',
+              'yolov8_n_syncbn_fast_8xb16-500e_coco',
+              'yolov8_s_syncbn_fast_8xb16-500e_coco',
+              'yolov8_n_mask-refine_syncbn_fast_8xb16-500e_coco'
+              ]
+
 
 def download_test_image():
     # Images
@@ -31,11 +55,25 @@ def download_test_image():
         'https://user-images.githubusercontent.com/59380685/266264600-9d0c26ca-8ba6-45f2-b53b-4dc98460c43e.jpg',
         'zidane.jpg')
 
+import shutil
 
-def download_cfg_checkpoint():
+def clear_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+    print(f"Clear {folder_path} successfully.")
+
+def download_cfg_checkpoint_model_name(model_name):
+    clear_folder("./checkpoint")
     download(package='mmyolo',
-             configs=['yolov5_s-v61_syncbn_fast_8xb16-300e_coco'],
-             dest_root='.')
+             configs=[model_name],
+             dest_root='./checkpoint')
 
 def detect_objects(args):
     config = args.config
@@ -105,7 +143,18 @@ def detect_objects(args):
             out_file=out_file,
             pred_score_thr=args.score_thr)
 
-def object_detection(img, config, checkpoint, out_dir, device, show, score_thr, class_name):
+
+def object_detection(img, model_name, out_dir, device, show, score_thr, class_name):
+    download_cfg_checkpoint_model_name(model_name)
+    # config = os.path.join('./checkpoint', config)
+    # checkpoint = os.path.join('./checkpoint', checkpoint)
+    path = "./checkpoint"
+    config = [f for f in os.listdir(path) if fnmatch.fnmatch(f, model_name + "*.py")][0]
+    config = path + "/" + config
+
+    checkpoint = [f for f in os.listdir(path) if fnmatch.fnmatch(f, model_name + "*.pth")][0]
+    checkpoint = path + "/" + checkpoint
+
     img_path = "input_img.jpg"
     img.save("input_img.jpg")
     args = Namespace(
@@ -119,16 +168,12 @@ def object_detection(img, config, checkpoint, out_dir, device, show, score_thr, 
         class_name=class_name,
     )
     detect_objects(args)
-    img_src = PIL.Image.open(img_path)
     img_out = PIL.Image.open(os.path.join(out_dir, img_path))
     return img_out
 
 inputs = [
     gr.inputs.Image(type="pil", label="input"),
-    gr.inputs.Textbox(
-        default="yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py", label="config"),
-    gr.inputs.Textbox(
-        default="yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth", label="checkpoint"),
+    gr.inputs.Dropdown(choices=[m for m in model_list], label='Model', default='yolov5_s-v61_syncbn_fast_8xb16-300e_coco'),
     gr.inputs.Textbox(default="./output", label="output"),
     gr.inputs.Radio(["cuda:0", "cpu"], default="cpu", label="device"),
     gr.inputs.Checkbox(default=False, label="show"),
@@ -136,14 +181,11 @@ inputs = [
     gr.inputs.Textbox(default=None, label="class_name"),
 ]
 download_test_image()
-download_cfg_checkpoint()
+
 examples = [
-    ['bus.jpg', 'yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py',
-     'yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth', './output', "cpu", False, 0.3,None],
-    ['dogs.jpg', 'yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py',
-     'yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth', './output', "cpu", False, 0.3,None],
-    ['zidane.jpg','yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py',
-     'yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth', './output', "cpu", False, 0.3,None]
+    ['bus.jpg', 'yolov5_n-v61_syncbn_fast_8xb16-300e_coco', './output', "cpu", False, 0.3, None],
+    ['dogs.jpg', 'yolov6_s_syncbn_fast_8xb32-400e_coco', './output', "cpu", False, 0.3, None],
+    ['zidane.jpg', 'rtmdet_tiny_syncbn_fast_8xb32-300e_coco', './output', "cpu", False, 0.3, None]
 ]
 
 text_output = gr.outputs.Textbox(label="输出路径")
@@ -151,9 +193,11 @@ src_image = gr.outputs.Image(type="pil")
 output_image = gr.outputs.Image(type="pil")
 
 title = "MMYOLO detection web demo"
-description = "use mmyolo detection"
-article = "<p style='text-align: center'><a href='https://github.com/open-mmlab/mmdetection'>MMDetection</a> 是一个开源的物体检测工具箱，提供了丰富的检测模型和数据增强方式，本项目基于 MMDetection 实现 Faster R-CNN 检测算法。</p>"
+description = "<p style='text-align: center'><a href='https://github.com/open-mmlab/mmyolo'>MMYOLO</a> 是一个开源的物体检测工具箱，提供了丰富的检测模型和数据增强方式。" \
+              "OpenMMLab YOLO series toolbox and benchmark. Implemented RTMDet, RTMDet-Rotated,YOLOv5, YOLOv6, YOLOv7, YOLOv8,YOLOX, PPYOLOE, etc.</p>"
+article = "<p style='text-align: center'><a href='https://github.com/open-mmlab/mmyolo'>MMDetection</a></p>"
 
 gr.Interface(fn=object_detection, inputs=inputs, outputs=output_image,
-             examples=examples, title=title,
+             examples=examples,
+             title=title,
              description=description, article=article, allow_flagging=False).launch()
