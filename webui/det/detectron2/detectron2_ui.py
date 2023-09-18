a5ded93ece235f762d846ddff1ff108442d6e302
isLinXu <2267379130@qq.com>
2023-09-18 14:26:21 +0800

    [webui][det][detectron2] update detectron2_ui.py and app.py

--- webui/det/detectron2/detectron2_ui.py
+++ webui/det/detectron2/detectron2_ui.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip install 'git+https://github.com/facebookresearch/detectron2.git'")
+os.system("pip install detectron2")
 import PIL.Image
 import gradio as gr
 import torch
