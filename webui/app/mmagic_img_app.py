596680bf3c3d9b2f6302beae82693603afc38920
isLinXu <2267379130@qq.com>
2023-09-20 11:27:55 +0800

    [webui][app] update mmagic_img_app.py

--- webui/app/mmagic_img_app.py
+++ webui/app/mmagic_img_app.py
@@ -1,10 +1,8 @@
 
 
 import os
-os.system("pip install 'mmengine>=0.6.0'")
 os.system("pip install 'mmcv>=2.0.0rc4,<2.1.0'")
-# os.system("pip install 'mmcv>=2.0.0'")
-# os.system("pip install 'mmengine'")
+os.system("pip install 'mmengine>=0.6.0'")
 os.system("pip install 'mmagic' -i https://pypi.tuna.tsinghua.edu.cn/simple")
 os.system("pip install albumentations -i https://pypi.tuna.tsinghua.edu.cn/simple")
 # os.system("pip install av")
