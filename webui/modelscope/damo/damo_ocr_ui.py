07bf3ab755e623bb5ccdb9a29216f83dca212304
isLinXu <2267379130@qq.com>
2023-09-23 15:53:05 +0800

    [webui][modelscope][damo] add damo_ocr_ui.py

---
+++ webui/modelscope/damo/damo_ocr_ui.py
@@ -0,0 +1,37 @@
+'''
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+import cv2
+
+ocr_recognition = pipeline(Tasks.ocr_recognition, model='damo/cv_convnextTiny_ocr-recognition-general_damo')
+
+### 使用url
+img_url = 'http://duguang-labelling.oss-cn-shanghai.aliyuncs.com/mass_img_tmp_20220922/ocr_recognition.jpg'
+result = ocr_recognition(img_url)
+print(result)
+'''
+
+import gradio as gr
+import torch
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+
+def ocr_recognition(img_url):
+    ocr_recognition = pipeline(Tasks.ocr_recognition, model='damo/cv_convnextTiny_ocr-recognition-general_damo')
+    result = ocr_recognition(img_url)
+    return result
+
+def download_test_image():
+    # Images
+    torch.hub.download_url_to_file(
+        'https://github.com/isLinXu/issues/assets/59380685/6cdbe53c-eb34-4310-8bd4-18b0a1aff803',
+        '../ocr_test.jpg')
+
+download_test_image()
+input_image = gr.inputs.Image()
+output_text = gr.outputs.Textbox()
+examples = [["ocr_test.jpg"]]
+
+gr.Interface(fn=ocr_recognition, inputs=input_image,
+             outputs=output_text, examples=examples,
+             title="OCR Recognition").launch()
