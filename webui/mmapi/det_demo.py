c6086c8a7921ab15f379065f89366bf9a4033d0c
isLinXu <2267379130@qq.com>
2024-04-24 16:05:27 +0800

    [webui][mmapi] add det_demo.py

--- webui/mmapi/det_demo.py
+++ webui/mmapi/det_demo.py
@@ -33,7 +33,16 @@ class ObjectDetector:
 
     def detect_objects(self, image_url):
         url = "https://platform.openmmlab.com/gw/model-inference/openapi/v1/detection"
-
+        algorithm_list = ["YOLOX", "Faster R-CNN", "Mask R-CNN", "Cascade R-CNN", "RetinaNet", "GHM",
+                          "Mask Scoring R-CNN", "Rethinking Classification and Localization for Object Detection",
+                          "ATSS", "AutoAssign", "CenterNet", "CentripetalNet", "CornerNet", "DCN", "Deformable DETR",
+                          "DetectoRS", "DETR", "Dynamic R-CNN", "Empirical Attention", "FCOS", "FoveaBox", "FP16",
+                          "FreeAnchor", "FSAF", "GCNet", "Generalized Focal Loss", "Group Normalization", "Grid R-CNN",
+                          "GRoIE", "Guided Anchoring", "HRNet", "InstaBoost", "Libra R-CNN", "LVIS", "NAS-FCOS",
+                          "NAS-FPN", "PAA", "PAFPN", "PISA", "PointRend", "RegNet", "RepPoints", "Res2Net", "ResNeSt",
+                          "SABL", "Rethinking ImageNet Pre-training", "seesaw_loss", "selfsup_pretrain", "Sparse R-CNN",
+                          "YOLOv3", "YOLACT", "VFNet", "SSD", "YOLOF", "Weight Standardization"]
+        backend_list = ["PyTorch", "TensorRT", "ONNXRuntime", "OpenPPL"]
         body = {
           "resource": image_url,
           "resourceType": "URL",
@@ -47,21 +56,35 @@ class ObjectDetector:
         }
 
         response = requests.post(url, headers=headers, json=body)
-        detection_data = response.json()["data"]["result"]["top_n_result"]
+        print("response: ", response.json())
 
-        image = np.asarray(bytearray(urlopen(image_url).read()), dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        if "data" in response.json():
+            status = response.json()["data"]["status"]
+            done = response.json()["data"]["done"]
+            if done == False:
+                print("msgCode: ", response.json()["msgCode"])
+                return None
+            else:
+                detection_data = response.json()["data"]["result"]["top_n_result"]
 
-        image = self.draw_detections(image, detection_data)
+                image = np.asarray(bytearray(urlopen(image_url).read()), dtype="uint8")
+                image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-        cv2.imshow("Result", image)
-        cv2.waitKey(0)
-        cv2.imwrite("output.jpg", image)
+                image = self.draw_detections(image, detection_data)
 
-        return detection_data
+                cv2.imshow("Result", image)
+                cv2.waitKey(0)
+                cv2.imwrite("output.jpg", image)
+                return detection_data
+        else:
 
-# 示例用法
-image_url = "https://oss.openmmlab.com/web-demo/static/one.e9be6cd7.jpg"
-detector = ObjectDetector()
-detections = detector.detect_objects(image_url)
-print(detections)
+
+            return None
+
+if __name__ == '__main__':
+    # 示例用法
+    image_url = "https://oss.openmmlab.com/web-demo/static/one.e9be6cd7.jpg"
+    # image_url = "https://img95.699pic.com/xsj/05/t8/v9.jpg"
+    detector = ObjectDetector()
+    detections = detector.detect_objects(image_url)
+    print(detections)
